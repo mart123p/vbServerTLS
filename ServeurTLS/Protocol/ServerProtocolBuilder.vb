e062b3afd2f51102361598a41e4c0b02b6e517bb
Martin Pouliot <martin.pouliot.01@edu.cegeptr.qc.ca>
2016-11-29 16:56:46 -0500

    Nearly done with the serverprotocolbuilder

--- ServeurTLS/Protocol/ServerProtocolBuilder.vb
+++ ServeurTLS/Protocol/ServerProtocolBuilder.vb
@@ -1,18 +1,27 @@
-﻿Public Class ServerProtocolBuilder
+﻿Imports Newtonsoft.Json.Linq
+
+Public Class ServerProtocolBuilder
 
     Public Function setInscription(ByVal id As String, ByVal status As ProtocolStatus) As String
+        Dim o As New JObject()
+        o.Add("id", id)
+        Return status & vbCrLf & o.ToString
     End Function
 
     Public Function setConnection(ByVal auth As String, ByVal status As ProtocolStatus) As String
-
+        Return status & vbCrLf & "AUTH: " & auth & vbCrLf
     End Function
 
     Public Function setStudyField(ByVal fields() As String, ByVal status As ProtocolStatus) As String
-
+        Dim a As New JArray()
+        For i = 0 To fields.Length - 1
+            a.Add(fields(i))
+        Next
+        Return status & vbCrLf & a.ToString & vbCrLf
     End Function
 
     Public Function setDisconnect(ByVal status As ProtocolStatus) As String
-
+        Return status & vbCrLf
     End Function
 
     Public Function setStudentDirectory(ByVal etudiant() As Etudiants, ByVal status As ProtocolStatus) As String
@@ -20,6 +29,6 @@
     End Function
 
     Public Function setProfile(ByVal status As ProtocolStatus) As String
-
+        Return status & vbCrLf
     End Function
 End Class
