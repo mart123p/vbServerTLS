2784c9323b8a47bcb830c9728c2cdcc931dc2610
Martin Pouliot <martin.pouliot.01@edu.cegeptr.qc.ca>
2016-11-29 17:43:16 -0500

    Done for the day

--- ServeurTLS/Protocol/ClientProtocolReader.vb
+++ ServeurTLS/Protocol/ClientProtocolReader.vb
@@ -1,7 +1,11 @@
 ﻿Public Class ClientProtocolReader
     Public Function getInscription(ByVal request As String) As User
         Dim requestArray As String() = request.Split(vbCrLf)
+        If requestArray.Length = 2 Then
 
+        Else
+            Throw New BadRequestException("POST /user" & vbCrLf & "jsonObject")
+        End If
 
     End Function
 End Class
---
+++ ServeurTLS/Protocol/User.vb
@@ -0,0 +1,23 @@
+﻿Public Class User
+    Inherits Etudiants
+    Dim _birthday As String
+    Dim _password As String
+    Public Sub New(ByVal firstName As String, ByVal lastName As String, ByVal email As String, ByVal studyField As String, ByVal birthday As String, ByVal password As String)
+        MyBase.New(firstName, lastName, email, studyField)
+        _birthday = birthday
+        _password = password
+    End Sub
+    Public Sub setBirthday(ByVal birthday As String)
+        _birthday = birthday
+    End Sub
+    Public Sub setPassword(ByVal password As String)
+        _password = password
+    End Sub
+    Public Function getBirthday() As String
+
+        Return _birthday
+    End Function
+    Public Function getPassword() As String
+        Return _password
+    End Function
+End Class
