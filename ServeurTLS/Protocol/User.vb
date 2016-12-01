1bc94c6ab4589e0f75f95b65a521bf78c054fe99
Yan-Eric Cossette <yan.eric.cossette.01@edu.cegeptr.qc.ca>
2016-12-01 14:18:09 -0500

    user changed

--- ServeurTLS/Protocol/User.vb
+++ ServeurTLS/Protocol/User.vb
@@ -2,6 +2,7 @@
     Inherits Etudiants
     Dim _birthday As String
     Dim _password As String
+    Dim _auth As String
     Public Sub New(ByVal firstName As String, ByVal lastName As String, ByVal email As String, ByVal studyField As String, ByVal birthday As String, ByVal password As String)
         MyBase.New(firstName, lastName, email, studyField)
         _birthday = birthday
@@ -13,6 +14,12 @@
     Public Sub setPassword(ByVal password As String)
         _password = password
     End Sub
+    Public Sub setAuth(ByVal auth As String)
+        _Auth = auth
+    End Sub
+    Public Function getAuth() As String
+        Return _auth
+    End Function
     Public Function getBirthday() As String
 
         Return _birthday
